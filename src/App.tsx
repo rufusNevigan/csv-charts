06bb19ab59a023f45ec9cd51b481f67a57780fda
Ruairidh Finnigan <ruairidh@nevigan.com>
2025-04-30 13:54:33 +1000

    feat(step 1.1): replace boilerplate with minimal App + heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <div>
-      <h1>CSV Charts</h1>
-    </div>
+    <main className="h-screen flex items-center justify-center bg-slate-100">
+      <h1 className="text-3xl font-semibold">CSV Charts</h1>
+    </main>
   );
 }
 
